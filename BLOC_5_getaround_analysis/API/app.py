35fb8fecac9e704916cf4aef77b2a6bf95080ee9
Souhail El Mousaddeq <swellisgood@gmail.com>
2023-02-06 07:01:18 +0100

    additional fixes + joblib added

--- BLOC_5_getaround_analysis/API/app.py
+++ BLOC_5_getaround_analysis/API/app.py
@@ -2,15 +2,23 @@ from fastapi import FastAPI, Request
 import uvicorn
 from pydantic import BaseModel, validator
 from typing import  Union
-import joblib
+from joblib import load
 import json
 import pandas as pd 
 
 app = FastAPI(
 title="Getaround API",
-description="""You can use this API to estimate the rental price of a car.
+description="""
+![GetAround](https://lever-client-logos.s3.amazonaws.com/2bd4cdf9-37f2-497f-9096-c2793296a75f-1568844229943.png)
+\n\n
+[GetAround](https://www.getaround.com/?wpsrc=Google+Organic+Search) is the Airbnb for cars. You can rent cars from any person for a few hours to a few days! Founded in 2009,
+this company has known rapid growth. In 2019, they count over 5 million users and about 20K available cars worldwide.
+\n\n
+The goal of Getaround API is to serve data that help users estimate daily rental value of their car.
+You can use this API to estimate the rental price of a car.
 The API is based on a dataset of cars from Getaround.
 The dataset contains 10,000 rows and 9 columns.
+
 The columns are:
 - `model_key`: the brand of the car (Toyota, BMW, Ford, etc.)
 - `mileage`: the mileage of the car (in km)
@@ -26,6 +34,7 @@ The columns are:
 - `has_speed_regulator`: whether the car has a speed regulator or not (boolean)
 - `winter_tires`: whether the car has winter tires or not (boolean)
 - `rental_price_per_day`: the rental price of the car (in $)
+
 The API has 6 endpoints:
 - **/preview**: returns a preview of the dataset (as a dictionary)
 - **/predict**: returns the predicted price of a car
@@ -33,14 +42,21 @@ The API has 6 endpoints:
 - **/groupby**: returns the grouped data of a column (as a dictionary)
 - **/filter-by**: returns the filtered data of a column (as a dictionary)
 - **/quantile**: returns the quantile of a column (as a float or string)
-The API is based on the FastAPI framework.
-"""
+
+
+The API is based on the FastAPI framework.,
+
+""",
+contact={
+        "name": "Souhail EL MOUSADDEQ",
+        "url": "https://github.com/Swellisgood",
+    },
 
 )
 
 @app.get("/")
 async def root():
-    message = """Welcome to the Getaround API. Append /docs to this address to see the documentation for the API on the Pricing dataset."""
+    message = """Welcome to the Getaround API. Add /docs to the end of this address to see the documentation for the API on the Pricing dataset."""
     return message
 
 
@@ -105,36 +121,38 @@ class Features(BaseModel):
 @app.post("/predict")
 async def predict(features:Features):
     """Get the predicted price of a car. 
-Example of input:
-{
-  "model_key": "Toyota",
-  "mileage": 25000,
-  "engine_power": 130,
-  "fuel": "diesel",
-  "paint_color": "red",
-  "car_type": "sedan",
-  "private_parking_available": true,
-  "has_gps": true,
-  "has_air_conditioning": true,
-  "automatic_car": false,
-  "has_getaround_connect": true,
-  "has_speed_regulator": true,
-  "winter_tires": true
-}
-Should return : "prediction": 143.557057416081
-All entries are case sensitive. List of possible values for categorical columns are available in the /unique-values endpoint.
-Wrong values will return a specific error message."""
+    Example of input:
 
-    features = dict(features)
-    input_df = pd.DataFrame(columns=['model_key', 'mileage', 'engine_power', 'fuel', 'paint_color','car_type', 'private_parking_available', 'has_gps',
-       'has_air_conditioning', 'automatic_car', 'has_getaround_connect','has_speed_regulator', 'winter_tires'])
-    input_df.loc[0] = list(features.values())
-    # Load the model & preprocessor
-    model = joblib.load('gbr_model.pkl')
-    prep = joblib.load('preprocessor.pkl')
-    X = prep.transform(input_df)
-    pred = model.predict(X)
-    return {"prediction" : pred[0]}
+    {
+    "model_key": "Toyota",
+    "mileage": 25000,
+    "engine_power": 130,
+    "fuel": "diesel",
+    "paint_color": "red",
+    "car_type": "sedan",
+    "private_parking_available": true,
+    "has_gps": true,
+    "has_air_conditioning": true,
+    "automatic_car": false,
+    "has_getaround_connect": true,
+    "has_speed_regulator": true,
+    "winter_tires": true
+    }
+
+    Should return : "prediction": 143.557057416081
+
+    All entries are case sensitive. List of possible values for categorical columns are available in the /unique-values endpoint.
+    Wrong values will return a specific error message."""
+    
+    # Read data 
+    data = pd.DataFrame(dict(features), index=[0])
+    # Load model
+    loaded_model = load('gbr_model.joblib')
+    #Prediction
+    prediction = loaded_model.predict(data)
+    #Load response
+    response ={"predictions": prediction.tolist()[0]}
+    return response
 
 
 # Endpoints to explore the dataset
@@ -149,7 +167,9 @@ async def preview(rows: int):
 @app.get("/unique-values")
 async def get_unique(column: str):
     """ Get unique values by given column : Input name of column (string).
+
     Example suffix : /unique-values?column=model_key
+
     N.B.: Attempting to get unique values for the rental_price_per_day or mileage column will return an error."""
     data = pd.read_csv('df_pricing.csv')
     selection = data[column].unique()
@@ -158,6 +178,7 @@ async def get_unique(column: str):
 @app.get("/groupby")
 async def groupby_agg(column:str,parameter:str):
     """ Get data grouped by given column : Input parameters are 1) column (string), 2) aggregation parameter (string).
+
     Example suffix : /groupby?column=model_key&parameter=mean"""
     data = pd.read_csv('df_pricing.csv')
     data_group = data.groupby(column).agg(parameter)
@@ -166,6 +187,7 @@ async def groupby_agg(column:str,parameter:str):
 @app.get("/filter-by")
 async def get_filtered(column:str,category:str):
     """ Get filtered data for given column : Input parameters are 1) column (string), 2) category (string).
+
     Example suffix : /filter-by?column=model_key&category=Toyota"""
     data = pd.read_csv('df_pricing.csv')
     filtered = data.loc[data[column] == category]
@@ -174,7 +196,9 @@ async def get_filtered(column:str,category:str):
 @app.get("/quantile")
 async def get_quantile(column:str,decimal:float):
     """Get quantile for given column : Input parameters are 1) column (string), 2) quantile (float between 0 and 1, ex : 0.2). 
+
     The interpolation method used when the desired quantile is between 2 data points is 'nearest' for categorical data and 'linear' for numerical data.
+
     Example suffix : /quantile?column=mileage&decimal=0.75"""
     data = pd.read_csv('df_pricing.csv')
     try:
